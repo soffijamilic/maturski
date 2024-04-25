ac23b865ebfb357a14b8177ade46a6bd9dda44e9
Sofija Milic <sofiajmilic2005@gmail.com>
2024-04-25 14:53:42 +0200

    kreiranje toyota stranice

--- app.py
+++ app.py
@@ -3,14 +3,17 @@ from flask import Flask,request,render_template
 app = Flask(__name__)
 
 @app.route('/')
-def root():
-    return 'Pocetna.'
-@app.route('/home')
 def home():
     return render_template("index.html")
 
 @app.route('/honda')
 def honda():
     return render_template("honda.html")
+
+@app.route('/toyota')
+def toyota():
+    return render_template("toyota.html")
+
+
 app.run()
 
