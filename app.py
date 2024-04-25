034d9f1983522502baf00cba1617ff44ce9a3654
Sofija Milic <sofiajmilic2005@gmail.com>
2024-04-25 14:48:16 +0200

    dodavanje nove stranice

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
-from flask import Flask
+from flask import Flask,request,render_template
 
 app = Flask(__name__)
 
-
+@app.route('/')
+def root():
+    return 'Pocetna.'
 @app.route('/home')
 def home():
-    return 'Ovo je home page.'
+    return render_template("index.html")
+
+@app.route('/honda')
+def home():
+    return render_template("index.html")
 app.run()
+
