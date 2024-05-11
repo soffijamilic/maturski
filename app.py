16d7ac6f8c32ae4a092bd1fcadf12200a21a26b4
Sofija Milic <sofiajmilic2005@gmail.com>
2024-05-11 14:09:34 +0200

    logo

--- app.py
+++ app.py
@@ -2,10 +2,14 @@ import os
 from flask import Flask,request,render_template
 
 app = Flask(__name__)
+app.config['LOGO_FOLDER'] = 'static/slike'
 
 @app.route('/')
 def home():
-    return render_template("index.html")
+    return render_template("index.html",logo_path="loggo.png")
+
+if __name__ == '__main__':
+    app.run(debug=True)
 
 @app.route('/honda')
 def honda():
